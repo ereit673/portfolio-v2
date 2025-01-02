65cceedb013ddeff45bec4457931c5cb9493a2d8
Alex Eremie <146241194+ereit673@users.noreply.github.com>
2025-01-02 09:23:33 +0100

    added hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { MainContentComponent } from "./main-content/main-content.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
