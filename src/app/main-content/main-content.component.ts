a38279b2100200ed20ee9411652b3f43d6ac3206
Alex Eremie <146241194+ereit673@users.noreply.github.com>
2025-01-02 13:09:17 +0100

    added featured section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeroComponent } from "./hero/hero.component";
 import { AboutComponent } from "./about/about.component";
 import { FeaturedComponent } from "./featured/featured.component";
+import { SkillsComponent } from "./skills/skills.component";
 
 @Component({
   selector: 'app-main-content',
-  imports: [HeroComponent, AboutComponent, FeaturedComponent],
+  imports: [HeroComponent, AboutComponent, FeaturedComponent, SkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
