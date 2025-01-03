96146ce3e35b5f89ab3c75f7752489eb3238f7b4
Alex Eremie <146241194+ereit673@users.noreply.github.com>
2025-01-03 11:36:12 +0100

    footer added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MainContentComponent } from "./main-content/main-content.component";
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, MainContentComponent],
+  imports: [RouterOutlet, HeaderComponent, MainContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,10 +4,11 @@ import { AboutComponent } from "./about/about.component";
 import { FeaturedComponent } from "./featured/featured.component";
 import { SkillsComponent } from "./skills/skills.component";
 import { ProjectsComponent } from "./projects/projects.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-main-content',
-  imports: [HeroComponent, AboutComponent, FeaturedComponent, SkillsComponent, ProjectsComponent],
+  imports: [HeroComponent, AboutComponent, FeaturedComponent, SkillsComponent, ProjectsComponent, ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
